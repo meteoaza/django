29164fe8dfe49a21accf586d8210227cb4c4cdd7
meteoaza <meteoaza@gmail.com>
2019-07-15 10:50:57 +0000

    15-07-19-1.2

--- maint/mainPage/urls.py
+++ maint/mainPage/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
-
 urlpatterns = [
     path('', views.index, name='index'),
+    path('sens/', views.sens, name='sens'),
     path('report/', views.report, name='report'),
     path('log/', views.log, name='log'),
-    path('clear/', views.clear, name='clear')
+    path('clear_log/', views.clear_log, name='clear'),
+    path('clear_rep/', views.clear_rep, name='clear'),
 ]
