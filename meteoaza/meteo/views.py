52e0b9f2957d9b8f5b9a015fe62e7316c9717839
meteoaza <meteoaza@gmail.com>
2019-07-19 10:20:10 +0000

    19-07-19-1.0ed

--- meteoaza/meteo/views.py
+++ meteoaza/meteo/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    with open('/home/meteoaza/github/django/MD_RW1.TEK', 'r', encoding="windows-1252")as f:
+    with open(r'z:\tek\dat_md\MD_RW1.TEK', 'r', encoding="windows-1252")as f:
         l = f.read().split(';')
     context = {'temp': l[27][2:], 'pres': l[24][2:], 'hum': l[28][2:]}
     return render(request, 'meteo/homePage.html', context)
 
 def sens(request):
-    with open('/home/meteoaza/github/django/MD_RW1.TEK', 'r', encoding="windows-1252")as f:
+    with open(r'c:\Users\Meteoaza\github\django\MD_RW1.TEK', 'r', encoding="windows-1252")as f:
         l = f.read().split(';')
     context = {'md1': l[12][2:], 'md2': l[47][2:]}
     return render(request, 'meteo/sens.html', context)
