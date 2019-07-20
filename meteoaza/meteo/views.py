a8c082cd6b9b588ac8bc4d9ecfcf62feb5270dbd
meteoaza <meteoaza@gmail.com>
2019-07-20 18:03:02 +0600

    20-07-19-1.0

--- meteoaza/meteo/views.py
+++ meteoaza/meteo/views.py
@@ -2,15 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    with open(r'z:\tek\dat_md\MD_RW1.TEK', 'r', encoding="windows-1252")as f:
+    # with open(r'z:\tek\dat_md\MD_RW1.TEK', 'r', encoding="windows-1252")as f:
+    with open('/home/meteoaza/github/django/MD_RW1.TEK', 'r', encoding="windows-1252")as f:
         l = f.read().split(';')
-    context = {'temp': l[27][2:], 'pres': l[24][2:], 'hum': l[28][2:]}
+    context = {'temp': l[27][2:], 'pres': l[24][2:], 'hum': l[28][2:], 'wind1': l[38][2:-2], 'wind2': l[39][2:],}
     return render(request, 'meteo/homePage.html', context)
 
 def sens(request):
-    with open(r'c:\Users\Meteoaza\github\django\MD_RW1.TEK', 'r', encoding="windows-1252")as f:
-        l = f.read().split(';')
-    context = {'md1': l[12][2:], 'md2': l[47][2:]}
+    try:
+        with open(r'c:\Users\Meteoaza\github\django\MD_RW1.TEK', 'r', encoding="windows-1252")as f:
+            l = f.read().split(';')
+        context = {'temp': l[27][2:], 'pres': l[24][2:], 'hum': l[28][2:]}
+    except:
+        context = {'temp': '----', 'pres': '----', 'hum': '----'}
     return render(request, 'meteo/sens.html', context)
 
 def news(request):
